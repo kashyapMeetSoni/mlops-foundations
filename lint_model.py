47cd9beeeb896732b8a9ceb04423171fdc1ed188
Meet soni <meetsoni1998@gmail.com>
2025-01-24 18:31:01 +0530

    Update lint_model.py

--- lint_model.py
+++ lint_model.py
@@ -3,7 +3,7 @@
 import pylint.lint
 
 # Specify the files you want to lint
-files_to_lint = ["model.py"]
+files_to_lint = ["model.py", "test_model.py"]
 
 # Run pylint
 pylint.lint.Run(files_to_lint, exit=False)
